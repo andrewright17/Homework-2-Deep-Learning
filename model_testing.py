eb80733661883aea5fc5a523359edb30e45072ab
andrewright17 <andrew98wright@gmail.com>
2024-03-01 00:54:00 -0500

    commit save

---
+++ model_testing.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import numpy as np
+import torch
+from torch.autograd import Variable
+import json
+from torch.utils.data import DataLoader, Dataset
+from bleu_eval import BLEU
+import pickle
+
+class test_data(Dataset):
+    def __init__(self, test_data_path):
+        self.avi = []
+        files = os.listdir(test_data_path)
+        for file in files:
+            key = file.split('.npy')[0]
+            value = np.load(os.path.join(test_data_path, file))
+            self.avi.append([key, value])
+    def __len__(self):
+        return len(self.avi)
+    def __getitem__(self, idx):
+        return self.avi[idx]
+    
+def testing( model, test_loader, i2w, device):
+    model.eval()
+    ss = []
+    for _, batch in enumerate(test_loader):
+        id, avi_feats = batch
+        avi_feats = avi_feats.to(device)
+        id, avi_feats = id, Variable(avi_feats).float()
+
+        _, seq_predictions = model(avi_feats, mode='inference')
+        test_predictions = seq_predictions
+        
+        result = [[i2w[x.item()] if i2w[x.item()] != '<UNK>' else 'something' for x in s] for s in test_predictions]
+        result = [' '.join(s).split('<EOS>')[0] for s in result]
+        rr = zip(id, result)
+        for r in rr:
+            ss.append(r)
+    return ss
+
+def main():
+    dataset = test_data('{}'.format(sys.argv[1]))
+    testing_loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=8)
+    with open('i2w.pickle', 'rb') as f:
+        i2w = pickle.load(f)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.load('modelv1_seq2seq.h5', map_location=lambda storage, loc: storage)
+    model = model.to(device)
+
